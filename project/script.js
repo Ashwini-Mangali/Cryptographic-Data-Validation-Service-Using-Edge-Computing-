88d5f3e158d877e12aa7e8ae650235212b26f4a3
Ashwini-Mangali <136227274+Ashwini-Mangali@users.noreply.github.com>
2025-02-09 14:48:42 +0530

    Delete project/script.js

--- project/script.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-    let inputData = document.getElementById("inputData").value;
-    if (inputData.trim() === "") {
-        alert("Input cannot be empty!");
-        return false;
-    }
-    return true;
-}
-
